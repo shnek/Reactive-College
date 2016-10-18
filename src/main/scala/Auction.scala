9f03c1153675f528a67dc681abc5c357e5ee4bc7
shnek94 <shnek94@gmail.com>
2016-10-18 13:20:20 +0200

    Beta1

--- src/main/scala/Auction.scala
+++ src/main/scala/Auction.scala
@@ -1,6 +1,90 @@
-import akka.actor.Actor
-import akka.actor.Actor.Receive
+import akka.actor.{Actor, ActorRef}
+import akka.event.LoggingReceive
 
-class Auction extends Actor{
-  override def receive: Receive = ???
+import scala.concurrent.duration._
+
+class Auction(minPrice: BigInt) extends Actor{
+  import Message._
+  val system = akka.actor.ActorSystem("system")
+  import system.dispatcher
+  var currentPrice = 0 : BigInt
+  var buyer = self
+
+  system.scheduler.scheduleOnce(10 second){
+    self ! BidTimer
+  }
+
+  def Created : Receive = LoggingReceive {
+    case Bid(amount) if amount > currentPrice => {
+      currentPrice = amount
+      buyer = sender
+      sender ! Bidded
+      context become Activated
+    }
+    case Bid(amount) => sender ! CurrentPrice(currentPrice)
+    case BidTimer => {
+      system.scheduler.scheduleOnce(10 second){
+        self ! DeleteTimer
+      }
+      context become Ignored
+    }
+  }
+
+  def Activated : Receive = LoggingReceive {
+    case Bid(amount) if amount > currentPrice => {
+      currentPrice = amount
+      buyer = sender
+      sender ! Bidded
+    }
+    case Bid(amount) => sender ! CurrentPrice(currentPrice)
+    case BidTimer => {
+
+      system.scheduler.scheduleOnce(10 second){
+        buyer ! AuctionDone(buyer, currentPrice)
+        self ! DeleteTimer
+      }
+      context become Sold
+    }
+  }
+
+  def Ignored : Receive = LoggingReceive {
+    case Relist => {
+      system.scheduler.scheduleOnce(10 second){
+        self ! DeleteTimer
+      }
+      context become Created
+    }
+    case DeleteTimer => {
+      context.system.terminate
+    }
+  }
+
+  def Sold : Receive = LoggingReceive {
+    case DeleteTimer => {
+      context.system.terminate
+    }
+  }
+
+  override def receive = Created
+}
+
+
+object Message {
+  //inside auction messages:
+  case object Bidded
+  case object Relist
+  case object BidTimer
+  case object DeleteTimer
+
+  //buyer to auction messages:
+  case class Bid(amount: BigInt){
+    require(amount > 0)
+  }
+  //auction to buyer messages:
+  case class CurrentPrice(amount: BigInt){
+    require(amount > 0)
+  }
+
+  case class AuctionDone(winner: ActorRef, amount: BigInt)
+
 }
--- src/main/scala/Buyer.scala
+++ src/main/scala/Buyer.scala
@@ -1,6 +1,41 @@
-import akka.actor.Actor
-import akka.actor.Actor.Receive
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
-class Buyer(money: Int, ActorRef: List[Auction]) extends Actor{
-  override def receive: Receive = ???
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+case object Start
+
+class Buyer(money: BigInt, auctions: List[ActorRef]) extends Actor {
+
+  import Message._
+
+  var current = 1: BigInt
+
+  override def receive = {
+    case Start => {
+      auctions(0) ! Bid(current)
+      current += 1
+    }
+    case CurrentPrice(amount) => {
+      current = amount + 1
+      if (current < money) sender ! Bid(current)
+    }
+  }
+
+}
+
+object Main extends App {
+  val system = ActorSystem("Auctions")
+
+  val auctions : List[ActorRef] = List(3).map(
+    value => system.actorOf(Props(classOf[Auction], BigInt(value)), "auction" + value)
+  )
+
+  val buyers = List(40,50).map(
+    value => system.actorOf(Props(classOf[Buyer], BigInt(value), auctions), "buyer" + value)
+  )
+
+  buyers.foreach(ActorRef => ActorRef ! Start)
+
+  Await.result(system.whenTerminated, Duration.Inf)
 }
